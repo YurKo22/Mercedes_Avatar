b691bf6c862bb527bab3f940013060e6ee3705ef
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-04-30 20:45:31 +0300

    1.1.1
    Adaptive of Sections & footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ $(function () {
     dots: false,
     slidesToShow: 4,
     variableWidth: true,
-    prevArrow: '<img class="arrow arrow-left" src="/images/arrow-left.svg" alt="">',
-    nextArrow: '<img class="arrow arrow-right" src="/images/arrow-right.svg" alt="">',
+    prevArrow:
+      '<img class="arrow arrow-left" src="/images/arrow-left.svg" alt="">',
+    nextArrow:
+      '<img class="arrow arrow-right" src="/images/arrow-right.svg" alt="">',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 });
